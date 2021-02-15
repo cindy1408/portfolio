a6c04e26cdc0705987314bc6afce5f2f2dfe97de
Cindy Cheung <cindycheung1408@gmail.com>
2021-02-15 13:06:32 +0000

    First commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const project = document.querySelector('.project');
+const aboutMe= document.querySelector('.aboutMe');
+const skills= document.querySelector('.skills');
+const main= document.querySelector('.main');
+
+
+project.addEventListener("click",)
